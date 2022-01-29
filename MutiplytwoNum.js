0857f30a85d9c656ebb61b99084965118d7689ef
robbaajaykumar <81790627+robbaajaykumar@users.noreply.github.com>
2022-01-29 17:58:52 +0530

    Add files via upload

---
+++ MutiplytwoNum.js
@@ -0,0 +1,5 @@
+var num1 =24;
+var num2 = 34;
+var multi = num1 * num2;
+
+console.log(multi);
---
+++ Swap2num.js
@@ -0,0 +1,7 @@
+var a =12;
+var b =25;
+console.log('before swapping '+ 'value of a '+a  + ' value of b' +b);
+var temp = a;
+a=b;
+b=temp
+console.log('after swapping '+  'value of a '+a  + ' value of b' +b);
